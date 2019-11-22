7b63dc570696acf17e1487473d8452ab331eaccb
fanyu.zhang <fanyu.zhang@beibei.com>
2019-11-22 19:52:53 +0800

    4 to n

--- DQN.py
+++ DQN.py
@@ -102,7 +102,7 @@ class DQN:
         y = []
         for i in range(self.batch_size):
             if done[i] == True:
-                y.append(-1000)
+                y.append(0)
             else:
                 y.append(reward[i] + self.sigema * maxQNext[i])
 
